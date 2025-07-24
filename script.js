15176d733e3b1972abc438f559e09305d73d2147
Samvel Zaqaryan <samvel.zaqaryan@reviro.co>
2025-07-24 19:13:15 +0400

    removed hearts

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const windowSize = window.innerWidth;
 let namak, pesa, names, right_block;
 
+document.addEventListener("DOMContentLoaded", function () {
+    let audio = document.getElementById("audio");
+    // Try to play audio on page load (may be blocked by browser autoplay policy)
+    audio.play().catch(() => {
+        // If autoplay fails, play on first user interaction
+        const playAudio = () => {
+            audio.play();
+            document.removeEventListener("click", playAudio);
+        };
+        document.addEventListener("click", playAudio);
+    });
+});
 
 if (windowSize > 1000) {
     document.querySelector('body').removeChild(document.querySelector('.small_container'))
